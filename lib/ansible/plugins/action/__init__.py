lifanov/ansible

commit 8647fdc7f25ba1c707c5333ca1581a1fa0ffe566

go back to defaulting wrapping commands in shell
this was taken out in an effort to default to the user's shell but creates issues as this is not known ahead of time
and its painful to set executable and shell_type for all servers, it should only be needed for those that restrict the user
to specific shells and when /bin/sh is not available. raw and command may still bypass this by explicitly passing None.
fixes #13882

still conditional

lib/ansible/plugins/action/__init__.py
@@ -475,8 +475,7 @@ class ActionBase(with_metaclass(ABCMeta, object)):
         display.debug("done with _execute_module (%s, %s)" % (module_name, module_args))
         return data
 
-    def _low_level_execute_command(self, cmd, sudoable=True, in_data=None,
-            executable=None, encoding_errors='replace'):
+    def _low_level_execute_command(self, cmd, sudoable=True, in_data=None, executable=C.DEFAULT_EXECUTABLE, encoding_errors='replace'):
         '''
         This is the function which executes the low level shell command, which
         may be commands to create/remove directories for temporary files, or to
@@ -492,7 +491,7 @@ class ActionBase(with_metaclass(ABCMeta, object)):
         '''
 
         if executable is not None:
-            cmd = executable + ' -c ' + cmd
+            cmd = executable + ' -c ' + pipes.quote(cmd)
 
         display.debug("_low_level_execute_command(): starting")
         if not cmd:

test/units/plugins/action/test_action.py
@@ -49,7 +49,7 @@ class TestActionBase(unittest.TestCase):
 
         play_context.remote_user = 'apo'
         action_base._low_level_execute_command('ECHO', sudoable=True)
-        play_context.make_become_cmd.assert_called_once_with('ECHO', executable=None)
+        play_context.make_become_cmd.assert_called_once_with("/bin/sh -c ECHO", executable='/bin/sh')
 
         play_context.make_become_cmd.reset_mock()
 
@@ -58,6 +58,6 @@ class TestActionBase(unittest.TestCase):
         try:
             play_context.remote_user = 'root'
             action_base._low_level_execute_command('ECHO SAME', sudoable=True)
-            play_context.make_become_cmd.assert_called_once_with('ECHO SAME', executable=None)
+            play_context.make_become_cmd.assert_called_once_with("/bin/sh -c 'ECHO SAME'", executable='/bin/sh')
         finally:
             C.BECOME_ALLOW_SAME_USER = become_allow_same_user

commit 669678830a949d31634c787716f6fb3b565e6ccb

fix become_method='su' regression introduced in 8647fdc
Quoting was wrong when trying to triple-stack quoted strings:

/bin/sh -c 'su  root -c "/bin/sh -c '"'"'echo BECOME-SUCCESS-ykpzytqwvnnelwkxgubbqkakdqrjfnyy; /bin/sh -c '"'"'"'"'"'"'"'"'LANG=C LC_ALL=C LC_MESSAGES=C /usr/local/bin/python2.7 /home/lifanov/.ansible/tmp/ansible-tmp-1453730839.89-43020615024025/ping; rm -rf "/home/lifanov/.ansible/tmp/ansible-tmp-1453730839.89-43020615024025/" > /dev/null 2>&1'"'"'"'"'"'"'"'"''"'"'"'

The simplest fix is to reset executable = None in the 'su' case, as this
fixes both this and the original problems fixed by 8647fdc.

Tested on: FreeBSD, with both become_method='sudo' and become_method='su'

lib/ansible/plugins/action/__init__.py
@@ -505,6 +505,9 @@ class ActionBase(with_metaclass(ABCMeta, object)):
             replacement strategy (python3 could use surrogateescape)
         '''
 
+        if self._play_context.become_method == 'su':
+            executable = None
+
         if executable is not None:
             cmd = executable + ' -c ' + pipes.quote(cmd)
 